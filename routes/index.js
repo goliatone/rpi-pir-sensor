ce971cb68f014aa2d364b48ff50d59dfa1f16af7
goliatone <burgosemi@gmail.com>
2016-02-01 14:01:18 -0500

    routes: Update index, autoregister

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+
+module.exports = function(app){
+    /* GET home page. */
+    router.get('/', function(req, res, next) {
+        res.render('index', { title: 'Express' });
+    });
+
+    app.use('/', router);
+};
