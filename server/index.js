3c36f6b961bbbe30c6cd3943985a23c8bfce68da
goliatone <burgosemi@gmail.com>
2016-02-01 15:33:30 -0500

    Update server index

--- server/index.js
+++ server/index.js
@@ -1,65 +1,100 @@
 'use strict';
+var EventEmitter = require('events');
+var _inherits = require('util').inherits;
+/**
+ * Module dependencies.
+ */
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = require('./setup');
+var debug = require('debug')('phonebooth-sensor:server');
+var http = require('http');
 
-var config = require('./config');
+/**
+ * Get port from environment and store in Express.
+ */
 
-var app = express();
+var port = normalizePort(process.env.PORT || process.env.NODE_APP_PORT || '3000');
+app.set('port', port);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+/**
+ * Create HTTP server.
+ */
 
-// uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-//Wire routes
-require('./routes/index')(app, config);
-require('./routes/config')(app, config);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+var server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+function App(){
+    EventEmitter.call(this);
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+_inherits(App, EventEmitter);
 
+App.prototype.init = function(opt){
+    server.listen(port);
+    server.on('error', this.onError.bind(this));
+    server.on('listening', this.onReady.bind(this));
+};
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+App.prototype.onReady = function(){
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+};
+
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+App.prototype.onError = function(error){
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+var app = new App();
+app.init();
 
 module.exports = app;
 
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
