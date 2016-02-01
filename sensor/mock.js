a8ed1ddf0353e2f4333ddc2d36adbf44edca4f18
goliatone <burgosemi@gmail.com>
2016-02-01 16:04:35 -0500

    sensor mock wip

--- sensor/mock.js
+++ sensor/mock.js
@@ -1,9 +1,35 @@
 'use strict';
 
+var express = require('express');
+var router = express.Router();
 
 
 module.exports = {
     init: function sensor$init(app, opts){
         console.log('MOCK ON!');
+
+        var presence = 0;
+        setInterval(function(){
+            console.log('Timeout');
+            var value = !! presence % 2;
+            app.emit('sensor.event', {
+                id: opts.id,
+                value: !! presence % 2,
+                type: 'motion' + (value ? 'start' : 'end')
+            });
+        }, 1000);
+        //This does not work :/ figure out, move to routes and do
+        //there.
+        router.get('/sensor', function(req, res){
+            console.log('HERE!');
+            app.emit('sensor.event', {
+                type: 'motionmock',
+                id: opts.id,
+                value: 23
+            });
+            res.send(200);
+        });
+
+        app.express.use('/mock', router);
     }
 };
