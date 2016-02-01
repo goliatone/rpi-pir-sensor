7cce52022fcebdddd21cda5874836a4027308ed2
goliatone <burgosemi@gmail.com>
2016-02-01 14:01:01 -0500

    app: wire routes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+//Wire routes
+require('./routes/index')(app);
+require('./routes/config')(app);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
