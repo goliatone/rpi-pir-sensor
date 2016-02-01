e7c958cb6fa8b8843a413ff5ad9a50706b195ec5
goliatone <burgosemi@gmail.com>
2016-02-01 16:23:05 -0500

    reporter: support dry run mode

--- reporter.js
+++ reporter.js
@@ -24,12 +24,20 @@ module.exports = {
         buffer.length = 0;
     },
     store: function(seriesName, points){
+        if(process.env.NODE_INFLUX_DRYRUN) {
+            return console.log('Influx:store', seriesName, points);
+        }
+
         client.writePoints(seriesName, points, function(err, res){
             if(err) console.error('Reporter Error: writePoints', err);
             else debug('Reporter: store OK', res);
         });
     },
     point: function(seriesName, values, tags){
+        if(process.env.NODE_INFLUX_DRYRUN) {
+            return console.log('Influx:point', seriesName, values, tags);
+        }
+
         client.writePoint(seriesName, values, tags, function(err, res){
             if(err) console.error('Reporter Error: writePoint', err);
             else debug('Reporter: store OK', res);
