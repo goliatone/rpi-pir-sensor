8db9a3bd27d87e7887d3b88f913964a21a1ee319
goliatone <burgosemi@gmail.com>
2016-03-30 16:05:32 -0400

    realtime wip

--- realtime/index.js
+++ realtime/index.js
@@ -1,6 +1,16 @@
 'use strict';
 
 
-module.exports = function(app, config){
-    //We should emit an amqp event here
+var ascoltatori = require('ascoltatori');
+
+
+module.exports = function(emitter, config){
+
+    ascoltatori.build(config, function (ascoltatore) {
+        console.log('===> AMQP client CONNECTED');
+        emitter.on('occupancy.change', function(data){
+            console.log('occupancy: publish event');
+            ascoltatore.publish('occupancy/change', data);
+        });
+    });
 };
