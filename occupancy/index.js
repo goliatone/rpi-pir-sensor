d6bbf9e612676c3b70cdd4a92bd3a5a583d940ff
goliatone <burgosemi@gmail.com>
2016-03-30 12:15:20 -0400

    adding occupancy brain

---
+++ occupancy/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+module.exports = function(options, config){
+    //initialize occupancy brain
+};
+
+
+module.exports.update = function(event){
+    //here, logic to detect occupancy.
+    //we want to trigger events on occupancy
+    //changes: occupied
+};
