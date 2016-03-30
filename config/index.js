6463b311c672df0beddb3cf57123264c7a834c28
goliatone <burgosemi@gmail.com>
2016-03-30 16:04:54 -0400

    config: added amqp opts

--- config/index.js
+++ config/index.js
@@ -97,5 +97,15 @@ module.exports = {
     logger: {
         //level: process.NODE_ENV === 'development' ? 'error' : 'warn',
         transports: transports
+    },
+    amqp: {
+        type: 'amqp',
+        json: true,
+        amqp: require('amqp'),
+        channel: 'webhook.github.*',
+        exchange: process.env.NODE_AMQP_EXCHANGE || ('wework.' + process.env.NODE_ENV),
+        client: {
+            url: process.env.NODE_AMQP_ENDPOINT
+        }
     }
 };
